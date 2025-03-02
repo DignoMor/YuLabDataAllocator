833cd32c4ef346574fc1050aab2285f82957ab86
Xiuqi Pan <xp76@cornell.edu>
2025-03-02 16:44:28 -0500

    Add sanity check for allocator to prohibit editing outside of configured drives

--- data_allocator/allocator.py
+++ data_allocator/allocator.py
@@ -15,6 +15,31 @@ class Allocator:
         self.config = ConfigHandler(config_path=config_path)
         self.storage = StorageManager(db_path=db_path)
 
+    def make_directory(self, path):
+        '''
+        Create a directory if it does not exist.
+        Also perform sanity checks.
+        '''
+        drive_paths = self.config.get_drive_paths().values()
+        if not np.any([path.startswith(drive_path) for drive_path in drive_paths]):
+            raise Exception(f"[ERROR] Path '{path}' is not in any of the configured drives.")
+
+        os.makedirs(path, exist_ok=True)
+    
+    def remove_directory(self, path):
+        '''
+        Remove a directory if it exists.
+        Also perform sanity checks.
+        '''
+        drive_paths = self.config.get_drive_paths().values()
+        if not np.any([path.startswith(drive_path) for drive_path in drive_paths]):
+            raise Exception(f"[ERROR] Path '{path}' is not in any of the configured drives.")
+
+        if os.path.exists(path):
+            shutil.rmtree(path)
+        else:
+            raise Exception(f"[ERROR] Path '{path}' does not exist.")
+
     def check_space(self):
         """
         Check available space on each drive.
@@ -39,7 +64,7 @@ class Allocator:
                                    )
 
         # Create the directory with all parent directories
-        os.makedirs(target_path, exist_ok=True)
+        self.make_directory(target_path)
 
         # Record the location
         self.storage.record_location(branch_name, target_drive)
@@ -62,9 +87,7 @@ class Allocator:
         Delete the branch and its record from storage.
         """
         path = self.get_path(branch_name)
-        if os.path.exists(path):
-            shutil.rmtree(path)
-            self.storage.delete_location(branch_name)
-        else:
-            raise Exception(f"[ERROR] No location found for '{branch_name}'")
+        self.remove_directory(path)
+        self.storage.delete_location(branch_name)
+        
         
