df03afabf2b88a9e770d71c8a5ece54e8a3a552f
Xiuqi Pan <xp76@cornell.edu>
2025-03-02 12:20:14 -0500

    Add ls command for entrypoint

--- YuLabDataAllocator.py
+++ YuLabDataAllocator.py
@@ -5,6 +5,8 @@ import sys
 import os
 
 from data_allocator.allocator import Allocator
+from data_allocator.storage_manager import StorageManager
+from data_allocator.tree_visualizer import TreeVisualizer
 
 CONFIG_PATH=os.path.join(os.environ.get("HOME"), ".YuLabDataAllocator", "config.json")
 DB_PATH=os.path.join(os.environ.get("HOME"), ".YuLabDataAllocator", "YuLabDataAllocator.db")
@@ -48,6 +50,13 @@ def delete_branch(branch_name):
     except Exception as e:
         raise e
 
+def ls_root(root): 
+    visualizer = TreeVisualizer(storage_manager=StorageManager(db_path=DB_PATH))
+    tree = visualizer.build_tree(root_branch=root)
+    output_str = TreeVisualizer.tree2str(tree)
+
+    sys.stdout.write(output_str)
+
 def main():
     """
     Main function to parse arguments and call the appropriate function.
@@ -70,6 +79,14 @@ def main():
     delete_parser = subparsers.add_parser("delete", help="Delete a branch and its record")
     delete_parser.add_argument("branch_name", type=str, help="Name of the branch to delete")
 
+    # ls command
+    ls_parser = subparsers.add_parser("ls", help="List all branches")
+    ls_parser.add_argument("--root", 
+                           type=str, 
+                           help="Root branch to list", 
+                           default="", 
+                           )
+
     # Parse arguments
     args = parser.parse_args()
 
@@ -80,6 +97,8 @@ def main():
         get_branch_path(args.branch_name)
     elif args.command == "delete":
         delete_branch(args.branch_name)
+    elif args.command == "ls":
+        ls_root(args.root)
     else:
         parser.print_help()
         sys.exit(1)
